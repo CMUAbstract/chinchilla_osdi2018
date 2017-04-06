13b7a64ec0f64dc71a20ce3f59065f9a91a0afc1
kwmaeng <kwmaeng91@gmail.com>
2017-04-06 07:25:29 -0400

    main code cleaned

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 
 #include <libwispbase/wisp-base.h>
-//#include <wisp-base.h>
 #include <libalpaca/alpaca.h>
 #include <libmspbuiltins/builtins.h>
 #include <libio/log.h>
@@ -20,25 +19,18 @@
 
 #include "pins.h"
 
-#define TEST_SAMPLE_DATA
-
 #define NIL 0 // like NULL, but for indexes, not real pointers
 
 #define DICT_SIZE         512
 #define BLOCK_SIZE         64
-
-#if 0 // These are largest Mementos with volatile vars can handle
-#define DICT_SIZE         280
-#define BLOCK_SIZE         16
-#endif
 
 #define NUM_LETTERS_IN_SAMPLE        2
 #define LETTER_MASK             0x00FF
 #define LETTER_SIZE_BITS             8
 #define NUM_LETTERS (LETTER_MASK + 1)
 
+// for timer
 unsigned overflow=0;
-//__attribute__((interrupt(TIMERB1_VECTOR))) 
 __attribute__((interrupt(51))) 
 	void TimerB1_ISR(void){
 		TBCTL &= ~(0x0002);
@@ -57,67 +49,52 @@ typedef unsigned letter_t;
 typedef unsigned sample_t;
 unsigned volatile *timer = &TBCTL;
 
-// NOTE: can't use pointers, since need to ChSync, etc
 typedef struct _node_t {
 	letter_t letter; // 'letter' of the alphabet
 	index_t sibling; // this node is a member of the parent's children list
 	index_t child;   // link-list of children
 } node_t;
 
-	TASK(1, task_init)
-	TASK(2, task_init_dict)
-	TASK(3, task_sample)
-	TASK(4, task_measure_temp)
-	TASK(5, task_letterize)
-	TASK(6, task_compress)
-	TASK(7, task_find_sibling)
-	TASK(8, task_add_node)
-	TASK(9, task_add_insert)
-	TASK(10, task_append_compressed)
-	TASK(11, task_print)
+TASK(1, task_init)
+TASK(2, task_init_dict)
+TASK(3, task_sample)
+TASK(4, task_measure_temp)
+TASK(5, task_letterize)
+TASK(6, task_compress)
+TASK(7, task_find_sibling)
+TASK(8, task_add_node)
+TASK(9, task_add_insert)
+TASK(10, task_append_compressed)
+TASK(11, task_print)
 TASK(12, task_done)
 
-	GLOBAL_SB(letter_t, letter);
-	GLOBAL_SB(unsigned, letter_idx);
-#ifdef TEST_SAMPLE_DATA
-	GLOBAL_SB(sample_t, prev_sample);
-#endif
-	GLOBAL_SB(index_t, out_len);
-	GLOBAL_SB(index_t, node_count);
-	GLOBAL_SB(node_t, dict, DICT_SIZE);
-	GLOBAL_SB(sample_t, sample);
-	GLOBAL_SB(index_t, sample_count);
-	GLOBAL_SB(index_t, sibling);
-	GLOBAL_SB(index_t, child);
-	GLOBAL_SB(index_t, parent);
-	GLOBAL_SB(index_t, parent_next);
-	GLOBAL_SB(node_t, parent_node);
-	GLOBAL_SB(node_t, compressed_data, BLOCK_SIZE);
-	GLOBAL_SB(node_t, sibling_node);
-	GLOBAL_SB(index_t, symbol);
-	//void write_to_gbuf(const void *value, void* data_addr, size_t var_size){
+GLOBAL_SB(letter_t, letter);
+GLOBAL_SB(unsigned, letter_idx);
+GLOBAL_SB(sample_t, prev_sample);
+GLOBAL_SB(index_t, out_len);
+GLOBAL_SB(index_t, node_count);
+GLOBAL_SB(node_t, dict, DICT_SIZE);
+GLOBAL_SB(sample_t, sample);
+GLOBAL_SB(index_t, sample_count);
+GLOBAL_SB(index_t, sibling);
+GLOBAL_SB(index_t, child);
+GLOBAL_SB(index_t, parent);
+GLOBAL_SB(index_t, parent_next);
+GLOBAL_SB(node_t, parent_node);
+GLOBAL_SB(node_t, compressed_data, BLOCK_SIZE);
+GLOBAL_SB(node_t, sibling_node);
+GLOBAL_SB(index_t, symbol);
 
-	//}
 static void init_hw()
 {
 	msp_watchdog_disable();
 	msp_gpio_unlock();
-	//	GPIO(1, DIR) |= BIT(1);
-	//	GPIO(1, DIR) |= BIT(2);
 	msp_clock_setup();
 }
 
 void init()
 {
-#if 0
-	TBCTL &= 0xE6FF; //set 12,11 bit to zero (16bit) also 8 to zero (SMCLK)
-	TBCTL |= 0x0200; //set 9 to one (SMCLK)
-	TBCTL |= 0x00C0; //set 7-6 bit to 11 (divider = 8);
-	TBCTL &= 0xFFEF; //set bit 4 to zero
-	TBCTL |= 0x0020; //set bit 5 to one (5-4=10: continuous mode)
-	TBCTL |= 0x0002; //interrupt enable
-	//	TBCTL &= ~(0x0020); //set bit 5 to zero(halt!)
-#endif
+	// only for timers
 	//	*timer &= 0xE6FF; //set 12,11 bit to zero (16bit) also 8 to zero (SMCLK)
 	//	*timer |= 0x0200; //set 9 to one (SMCLK)
 	//	*timer |= 0x00C0; //set 7-6 bit to 11 (divider = 8);
@@ -126,43 +103,31 @@ void init()
 	//	*timer |= 0x0002; //interrupt enable
 	//	*timer &= ~(0x0020); //set bit 5 to zero(halt!)
 	init_hw();
-	//	WISP_init();
 #ifdef CONFIG_EDB
-	//debug_setup();
 	edb_init();
 #endif
 
 	INIT_CONSOLE();
 	__enable_interrupt();
-	//set_dirty_buf(&data, &data_dest, &data_size);
-	//	set_dirty_buf();
 	PRINTF(".%u.\r\n", curctx->task->idx);
 }
 
 static sample_t acquire_sample(letter_t prev_sample)
 {
-	//letter_t sample = rand() & 0x0F;
 	letter_t sample = (prev_sample + 1) & 0x03;
 	return sample;
 }
-unsigned nvram=0;
+
 void task_init()
 {
 	LOG("init\r\n");
 	GV(parent_next) = 0;
-
 	LOG("init: start parent %u\r\n", GV(parent));
-
 	GV(out_len) = 0;
-
 	GV(letter) = 0;
-#ifdef TEST_SAMPLE_DATA
 	GV(prev_sample) = 0;
-#endif
 	GV(letter_idx) = 0;;
-
 	GV(sample_count) = 1;
-
 	TRANSITION_TO(task_init_dict);
 }
 
@@ -176,8 +141,7 @@ void task_init_dict()
 		.child = NIL, // init an empty list for children
 	};
 	int i = GV(letter);	
-	GV(dict, i) = node; // ------------------------------------->>>>BUG
-
+	GV(dict, i) = node; 
 	GV(letter)++;
 	if (GV(letter) < NUM_LETTERS) {
 		TRANSITION_TO(task_init_dict);
@@ -185,19 +149,15 @@ void task_init_dict()
 		GV(node_count) = NUM_LETTERS;
 		TRANSITION_TO(task_sample);
 	} 
-
 }
 
 void task_sample()
 {
-	//
-	//
 	LOG("sample: letter idx %u\r\n", GV(letter_idx));
 
 	unsigned next_letter_idx = GV(letter_idx) + 1;
 	if (next_letter_idx == NUM_LETTERS_IN_SAMPLE)
 		next_letter_idx = 0;
-
 
 	if (GV(letter_idx) == 0) {
 		GV(letter_idx) = next_letter_idx;
@@ -210,25 +170,13 @@ void task_sample()
 
 void task_measure_temp()
 {
-	//
-	//
-	//  TASK_PROLOGUE();
-
 	sample_t prev_sample;
-
-#ifdef TEST_SAMPLE_DATA
 	prev_sample = GV(prev_sample);
-#else
-	prev_sample = 0;
-#endif
 
 	sample_t sample = acquire_sample(prev_sample);
 	LOG("measure: %u\r\n", sample);
-
-#ifdef TEST_SAMPLE_DATA
 	prev_sample = sample;
 	GV(prev_sample) = prev_sample;
-#endif
 	GV(sample) = sample;
 	TRANSITION_TO(task_letterize);
 }
@@ -251,10 +199,6 @@ void task_letterize()
 
 void task_compress()
 {
-	//
-	//
-	// TASK_PROLOGUE();
-
 	node_t parent_node;
 
 	// pointer into the dictionary tree; starts at a root's child
@@ -267,19 +211,9 @@ void task_compress()
 	LOG("compress: parent node: l %u s %u c %u\r\n", parent_node.letter, parent_node.sibling, parent_node.child);
 
 	GV(sibling) = parent_node.child;
-
-	// Send a full node instead of only the index to avoid the need for
-	// task_add to channel the dictionary to self and thus avoid
-	// duplicating the memory for the dictionary (premature opt?).
-	// In summary: instead of self-channeling the whole array, we
-	// proxy only one element of the array.
-	//
-	// NOTE: source of inefficiency: we execute this on every step of traversal
-	// over the nodes in the tree, but really need this only for the last one.
-	GV(parent_node) = parent_node; //------->> BUG HERE??????
+	GV(parent_node) = parent_node;
 	GV(parent) = parent;
 	GV(child) = parent_node.child;
-
 	GV(sample_count)++;
 
 	TRANSITION_TO(task_find_sibling);
@@ -287,16 +221,11 @@ void task_compress()
 
 void task_find_sibling()
 {
-	//
-	//
-	// TASK_PROLOGUE();
-
 	node_t *sibling_node;
 
 	LOG("find sibling: l %u s %u\r\n", GV(letter), GV(sibling));
 
 	if (GV(sibling) != NIL) {
-		// See comments in task_add_node about this split. It's a memory optimization.
 		int i = GV(sibling);
 		sibling_node = &GV(dict,i); 
 
@@ -305,6 +234,7 @@ void task_find_sibling()
 
 		if (sibling_node->letter == GV(letter)) { // found
 			LOG("find sibling: found %u\r\n", GV(sibling));
+
 			GV(parent_next) = GV(sibling);
 
 			TRANSITION_TO(task_letterize);
@@ -318,40 +248,23 @@ void task_find_sibling()
 	} 
 	LOG("find sibling: not found\r\n");
 
-	// Reset the node pointer into the dictionary tree to point to the
-	// root's child corresponding to the letter we are about to insert
-	// NOTE: The cast here relies on the fact that root's children are
-	// initialized in by inserting them in order of the letter value.
 	index_t starting_node_idx = (index_t)GV(letter);
 	GV(parent_next) = starting_node_idx;
 
-	// Add new node to dictionary tree, and, after that, append the
-	// compressed symbol to the result
-	//
 	LOG("find sibling: child %u\r\n", GV(child));
+	
+
 	if (GV(child) == NIL) {
 		TRANSITION_TO(task_add_insert);
 	} else {
 		TRANSITION_TO(task_add_node); 
 	}
-	//  }
 }
 
 void task_add_node()
 {
-	//
-	//
-	// TASK_PROLOGUE();
-
 	node_t *sibling_node;
 
-	// This split is a memory optimization. It is to avoid having the
-	// channel from init task allocate memory for the whole dict, and
-	// instead to hold only the ones it actually modifies.
-	//
-	// NOTE: the init nodes do not come exclusively from the init task,
-	// because they might be later modified.
-	//if (sibling < NUM_LETTERS) {
 	int i = GV(sibling);
 	sibling_node = &GV(dict, i);
 
@@ -368,8 +281,6 @@ void task_add_node()
 		LOG("add node: found last\r\n");
 
 		node_t sibling_node_obj = *sibling_node;
-
-		//		GV(sibling) = GV(sibling);
 		GV(sibling_node) = sibling_node_obj;
 
 		TRANSITION_TO(task_add_insert);
@@ -378,8 +289,6 @@ void task_add_node()
 
 void task_add_insert()
 {
-	//
-	//
 	LOG("add insert: nodes %u\r\n", GV(node_count));
 
 	if (GV(node_count) == DICT_SIZE) { // wipe the table if full
@@ -396,31 +305,25 @@ void task_add_insert()
 	};
 
 	if (GV(parent_node).child == NIL) { // the only child
-
 		LOG("add insert: only child\r\n");
 
 		node_t parent_node_obj = GV(parent_node);
 		parent_node_obj.child = child;
-
 		int i = GV(parent);
 		GV(dict, i) = parent_node_obj;
 
 	} else { // a sibling
 
 		index_t last_sibling = GV(sibling);
-
 		node_t last_sibling_node = GV(sibling_node);                   
 
 		LOG("add insert: sibling %u\r\n", last_sibling);
-
+		
 		last_sibling_node.sibling = child;
-
 		GV(dict, last_sibling) = last_sibling_node;
 	}
 	GV(dict, child) = child_node;
-
 	GV(symbol) = GV(parent);
-
 	GV(node_count)++;
 
 	TRANSITION_TO(task_append_compressed);
@@ -428,8 +331,6 @@ void task_add_insert()
 
 void task_append_compressed()
 {
-	//
-	//
 	LOG("append comp: sym %u len %u \r\n", GV(symbol), GV(out_len));
 	int i = GV(out_len);
 	GV(compressed_data, i).letter = GV(symbol);
@@ -458,17 +359,11 @@ void task_print()
 	BLOCK_PRINTF("\r\n");
 	BLOCK_PRINTF("rate: samples/block: %u/%u\r\n", GV(sample_count), BLOCK_SIZE);
 	BLOCK_PRINTF_END();
-	//TRANSITION_TO(task_sample); // restart app
 	TRANSITION_TO(task_done); // for now just do one block
 }
 
 void task_done()
 {
-	//WATCHPOINT(1);
-#if TIME > 0
-	//	PRINTF("TIME end is 65536*%u+%u\r\n",overflow,(unsigned)TBR);
-
-#endif
 	TRANSITION_TO(task_init);
 }
 
