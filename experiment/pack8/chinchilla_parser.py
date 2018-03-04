27d91b6a2b59bedc6e8c7928c0c3fff5452ddd29
kwmaeng <kwmaeng91@gmail.com>
2018-03-04 16:23:56 -0500

    collecting measurements

---
+++ experiment/pack8/chinchilla_parser.py
@@ -0,0 +1,31 @@
+import fileinput
+import sys
+import re
+
+fileName = sys.argv[1]
+chkpts = {}
+runTime = -1
+
+f = open(fileName + "_processed", "w")
+iterCount = 0
+
+for line in fileinput.input(fileName):
+    start = re.search(r"(?P<name>[0-9]+\.[0-9]+): start:", line)
+    if start is not None:
+        if runTime != -1:
+            f.write("iter:" + str(iterCount) + " time:" + str(runTime))
+            f.write(" chkpt status:" + repr(chkpts) + "\n")
+            iterCount += 1
+        startTime = float(start.group("name"))
+
+    end = re.search(r"(?P<name>[0-9]+\.[0-9]+): end", line)
+    if end is not None:
+        endTime = float(end.group("name"))
+        runTime = endTime - startTime
+
+    chkpt = re.search(r"chkpt\[(?P<name1>[0-9]+)\]=(?P<name2>[0-9]+)", line)
+    if chkpt is not None:
+        chkptId = int(chkpt.group("name1"))
+        unrollFactor = int(chkpt.group("name2"))
+        chkpts[chkptId] = unrollFactor
+
