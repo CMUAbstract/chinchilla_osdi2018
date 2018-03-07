d468026812a5895cada87a397ffa7506988ade2e
kwmaeng <kwmaeng91@gmail.com>
2018-03-07 18:51:11 -0500

    timer data for paper

---
+++ experiment/timer_eval/parser.py
@@ -0,0 +1,37 @@
+import fileinput
+import sys
+import re
+
+def avg(li):
+    print(li)
+    result = 0;
+    for val in li:
+        result += val
+    result /= len(li)
+    return result
+
+fileName = sys.argv[1]
+
+values = []
+
+f = open(fileName + "_processed", "w")
+iterCount = 0
+
+for line in fileinput.input(fileName):
+    start = re.search(r"(?P<name>[0-9]+\.[0-9]+): start", line)
+    if start is not None:
+        startTime = float(start.group("name"))
+
+    end = re.search(r"(?P<name>[0-9]+\.[0-9]+): end", line)
+    if end is not None:
+        endTime = float(end.group("name"))
+        runTime = endTime - startTime
+        values.append(runTime)
+
+    timer = re.search(r"improve: [0-9]+ (?P<name>[0-9]+) [0-9]+", line)
+    if timer is not None:
+        interval = int(timer.group("name"))
+        f.write("time:" + str(runTime) + " interval:" + str(interval) + "\n")
+
+# Mean of last 5 vals
+f.write("mean:" + str(avg(values[-5:len(values)])));
